37d44f38bf0e54039ed6ceba0cb7dfcb2acc8291
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-11-02 18:11:49 +0200

    OOP_Lab_8.program.core.parser - Added tariff parser interface

---
+++ src/main/java/OOP_Lab_8/program/core/parser/ITariffParser.java
@@ -0,0 +1,11 @@
+package OOP_Lab_8.program.core.parser;
+
+import OOP_Lab_8.program.domain.entity.Tariff;
+import OOP_Lab_8.program.domain.exception.TariffParseException;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public interface ITariffParser {
+    ArrayList<Tariff> parse(File xmlFile) throws TariffParseException;
+}
